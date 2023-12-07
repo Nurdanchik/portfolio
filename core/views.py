ae3041ade6a32cb5ad0fe9f36c6803a377031383
Nurdanchik <nysanovnurdan33@gmail.com>
2023-12-07 22:32:48 +0600

    Changes in views.py

--- core/views.py
+++ core/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.decorators import login_required
 from .models import ContactMessage
 from .forms import ContactMessageForm
 
-@login_required
 def contact_view(request):
     if request.method == 'POST':
         form = ContactMessageForm(request.POST)
@@ -15,6 +14,7 @@ def contact_view(request):
 
     return render(request, 'index.html', {'form': form})
 
+@login_required
 def inbox(request):
     messages = ContactMessage.objects.all().order_by('-created_at')
     return render(request, 'inbox.html', {'messages': messages})
